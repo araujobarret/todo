d4e3710db239ebca52a3ea17d1df0bc5c7bdc797
Carlos Alberto <araujobarret@gmail.com>
2017-12-05 14:16:46 -0200

    Login, logout, middleware for routes implemented

--- app/app.jsx
+++ app/app.jsx
@@ -8,7 +8,16 @@ let actions = require('actions');
 let store = require('configureStore').configure();
 let TodoAPI = require('TodoAPI');
 import Login from 'Login';
+import firebase from 'app/firebase';
 
+firebase.auth().onAuthStateChanged((user) => {
+  if(user) {
+    hashHistory.push('/todos');
+  }
+  else {
+    hashHistory.push('/');
+  }
+});
 
 store.dispatch(actions.startAddTodos());
 
@@ -20,11 +29,25 @@ $(document).foundation();
 // App css
 require('style!css!sass!applicationStyles');
 
+let requireLogin = (nextState, replace, next) => {
+  if(!firebase.auth().currentUser) {
+    replace('/');
+  }
+  next();
+};
+
+let redirectIfLogin = (nextState, replace, next) => {
+  if(firebase.auth().currentUser) {
+    replace('/todos');
+  }
+  next();
+};
+
 ReactDOM.render(
   <Provider store={store}>
     <Router history={hashHistory}>
-      <Route path="/">
-        <Route path="todos" component={TodoApp} />
+      <Route path="/" onEnter={redirectIfLogin}>
+        <Route path="todos" component={TodoApp} onEnter={requireLogin}/>
         <IndexRoute component={Login}/>
       </Route>
     </Router>
